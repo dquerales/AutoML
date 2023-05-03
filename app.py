5a9958cdd2601f8a1564ca03eb3560b7f3929d5b
Daniel Querales <45567065+dquerales@users.noreply.github.com>
2023-05-03 15:54:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ if choice == "Profiling":
 if choice == "Modelling": 
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if st.button('Run Modelling', use_container_width=True): 
-        setup(df, target=chosen_target, silent=True)
+        setup(df, target=chosen_target)
         setup_df = pull()
         st.dataframe(setup_df)
         best_model = compare_models()
